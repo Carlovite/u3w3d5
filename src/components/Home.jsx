023dec60e885b1d82e41dd98ebe612bb030fe7f1
Carlo Vitetta <vitettacarlo@gmail.com>
2023-11-10 12:50:25 +0100

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import MainSection from "./MainSection";
+import NavbarBottom from "./NavbarBottom";
+import Sidebar from "./Sidebar";
+import "bootstrap/dist/css/bootstrap.min.css";
+const Home = () => {
+  <div className="container-fluid">
+    <div className="row">
+      <Sidebar></Sidebar>
+      <MainSection></MainSection>
+    </div>
+  </div>;
+  <NavbarBottom></NavbarBottom>;
+};
+export default Home;
